e2b2309f24e4dea2efb1521c6fed67d6e6b2fd05
gurudath <gurudath_daws>
2025-07-29 21:56:22 +0530

    commit

--- 9-specialVariables.sh
+++ 9-specialVariables.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
+echo "Print the current script name: $0"
 echo "Pint all the variable given during runtime: $@"
 echo "Print the number of arguments given during runtime: $#"
+echo "print the process id of current script: $$"
+echo "print the hostname: $HOSTNAME"
+echo "Print present working directory: $PWD"
+echo "Print the user who is running this script $USER"
+echo "Print the home directory of this user: $HOME"
+sleep 60 &
+echo "Print the process of last background process : $!"
