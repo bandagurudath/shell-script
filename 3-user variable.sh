aea8efb92ce29ffed89dafdf23dbf522e7b14c88
gurudath <gurudath_daws>
2025-07-29 21:31:20 +0530

    commit

---
+++ 3-user variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Ramesh
+Person2=Suresh
+
+echo "Hi $Person1 , How are yoy?"
+echo "Hello $Person2 , I am fine"
+echo "How is the work going on $Person2 ?"
+echo "Its going well $Person1" 
