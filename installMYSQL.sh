374ef73c0e6678cf74d0d89c61b6f3ecb4d2d89f
Gurudath <bandagurudath@gmail.com>
2025-07-31 22:26:56 +0530

    commit files

--- installMYSQL.sh
+++ installMYSQL.sh
@@ -4,10 +4,10 @@ userID=(id -u)
 
 if [ $userID -eq 0 ]
 then
-echo "Your are super user"
+    echo "Your are super user"
 else
-echo "Run with Super user"
-exit 1
+    echo "Run with Super user"
+    exit 1
 fi
 
 dnf install mysql -y
