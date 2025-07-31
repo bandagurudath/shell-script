30bdef4b2289bf99524f85f31aff4745ae3ebf37
Gurudath <bandagurudath@gmail.com>
2025-07-31 22:04:33 +0530

    commit files

--- Arrays.sh
+++ Arrays.sh
@@ -2,11 +2,6 @@
 
 Movies=(ABC,XYZ,123)
 
-#echo "Movie1: ${Movies[0]}
-#echo "Movie1: ${Movies[1]}
-#echo "Movie1: ${Movies[2]}
-
-for i in $Movies
-do
-echo "$Movies[i] /n"
-done
+echo "Movie1: ${Movies[0]}
+echo "Movie1: ${Movies[1]}
+echo "Movie1: ${Movies[2]}
