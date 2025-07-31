5bd27ca1fa8c221cd372135e5362e33eb07ed7b2
Gurudath <bandagurudath@gmail.com>
2025-07-31 21:54:48 +0530

    commit files

---
+++ HideInputValuesinCLI.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "What is your name"
+read UserName
+echo "Where are you from"
+read City
+read "Tell me your Password"
+read -s Password
+
+echo "UserName : $UserName from City: $City and Password: $Password"
