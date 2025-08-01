eff5b8e1c30ff95d0d83656faa1326f19ba611f1
Gurudath <bandagurudath@gmail.com>
2025-08-01 13:51:27 +0530

    comitting

---
+++ 15-findGreaterThreshholdFiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Storage=$(df -hT)
+Current_Usage=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+FIle_Name=$(df -hT | grep xfs | awk -F " " '{print $NF}')
+Threshold_Storage=30
+
+while IFS= read -r line
+do
+if [ $Current_Usage -gt $Threshold_Storage ]
+then
+echo "path storage $FIle_Name has exceeded $Threshold_Storage"
+else
+fi
+done <<< $Storage
